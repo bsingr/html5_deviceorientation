56429d2c9401d34fbac59172b6c23e77d2c7b4b0
Jens Bissinger <mail@jens-bissinger.de>
2015-10-10 21:11:23 +0200

    Visualize orientation

--- server.js
+++ server.js
@@ -6,16 +6,31 @@ var io = require('socket.io')(server);
 console.log("listening 4080");
 server.listen(4080);
 
+app.use('/', express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('visualize', {z: 1});
+var controllers = -1;
 
-  socket.on('devicemotion', function (data) {
-    // console.log(data);
-    socket.broadcast.emit('visualize', data);
+io.on('connection', function (socket) {
+  socket.on('registerController', function(data) {
+    controllers++;
+    controllers = controllers % 3;
+    var controller = controllers;
+    console.log(controller);
+
+    socket.emit('registeredController', controller);
+
+    socket.on('devicemotion', function (data) {
+      // console.log(data);
+      socket.broadcast.emit('visualizeDevicemotion', {controller: controller, data: data});
+    });
+
+    socket.on('deviceorientation', function (data) {
+      // console.log(data);
+      socket.broadcast.emit('visualizeDeviceorientation', {controller: controller, data: data});
+    });
   });
 });
