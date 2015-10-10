91733b88874c369da794c968b82230db73733e27
Jens Bissinger <mail@jens-bissinger.de>
2015-10-10 20:18:10 +0200

    Add graph visualization

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 console.log("listening 4080");
 server.listen(4080);
 
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
